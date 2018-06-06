b0d8f75ac1417193028bbe0df4c1bb77125ebdc8
meme <lemooo.smart@yahoo.com>
2018-06-07 01:15:51 +0300

    commit Base from ngAnim2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { trigger, style, state, transition, query, stagger, animate, keyframes, group } from '@angular/animations';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('bannerAnimation', [
+      transition(":increment", group([
+        query(':enter', [
+          style({ left: '100%' }),
+          animate('0.5s ease-out', style('*'))
+        ]),
+        query(':leave', [
+          animate('0.5s ease-out', style({ left: '-100%' }))
+        ])
+      ])),
+      transition(":decrement", group([
+        query(':enter', [
+          style({ left: '-100%' }),
+          animate('0.5s ease-out', style('*'))
+        ]),
+        query(':leave', [
+          animate('0.5s ease-out', style({ left: '100%' }))
+        ])
+      ]))
+    ])
+  ]
+
+
 })
 export class AppComponent {
-  title = 'app';
+  allBanners: string[] = ['1', '2', '3', '4'];
+  selectedIndex: number = 0;
+ 
+  get banners() {
+     return [this.allBanners[this.selectedIndex]];
+  }
+ 
+  previous() {
+    this.selectedIndex = Math.max(this.selectedIndex - 1, 0);
+  }
+ 
+  next() {
+    this.selectedIndex = Math.min(this.selectedIndex + 1, this.allBanners.length - 1);
+  }
 }
